b3e60869a094d3e057a1fe78abe24278df27feaa
Tzimis <tzimisargy@hotmail.com>
2016-09-27 16:07:52 +0300

    Add files via upload

---
+++ Askisi1.py
@@ -0,0 +1,10 @@
+for i in range(1,10):
+    j=i
+    t=0
+    while (t<9-j):
+        print("0",end='')
+        t += 1
+    while (t<9):
+        print(j, end='')
+        t += 1
+    print('')    
